9ef6079078291b47c8210420d7681c77a6d7622c
packergundo <packergundo@gmail.com>
2019-10-16 20:13:32 -0500

    Cleaned up, took out hardcoded paths

--- image-creator.sh
+++ image-creator.sh
@@ -5,10 +5,13 @@
 # I *do* include the current Portage directory in my image. Why 
 # waste the time doing a full sync? You're going to do it anyway
 
+# to use -- image-creator.sh <path to SD card>
+
 DATE=$(date +%F)
 
 IMAGE=gentoo-pi-$DATE.img
 STAGE4=stage4-gentoo-pi-$DATE
+BASEDIR=/home/gentoo-pi
 #BOOT=$11
 #GENTOO=$13
 
@@ -19,21 +22,21 @@ mount /dev/loop0p3 /mnt/gentoo
 mount /dev/loop0p1 /mnt/gentoo/boot
 rm -rf /mnt/gentoo/home/gundo
 rm -rf /mnt/gentoo/usr/portage/packages/*
-cp /root/gentoopi_files/passwd /mnt/gentoo/etc/passwd
-cp /root/gentoopi_files/shadow /mnt/gentoo/etc/shadow
-cp /root/gentoopi_files/group /mnt/gentoo/etc/group
-cp /root/gentoopi_files/gshadow /mnt/gentoo/etc/gshadow
-cp /root/gentoopi_files/sudoers /mnt/gentoo/etc/sudoers
-cp /root/gentoopi_files/resolv.conf /mnt/gentoo/etc/resolv.conf
-cp /root/gentoopi_files/net /mnt/gentoo/etc/conf.d/net
+cp $BASEDIR/files/passwd /mnt/gentoo/etc/passwd
+cp $BASEDIR/files/shadow /mnt/gentoo/etc/shadow
+cp $BASEDIR/files/group /mnt/gentoo/etc/group
+cp $BASEDIR/files/gshadow /mnt/gentoo/etc/gshadow
+cp $BASEDIR/files/sudoers /mnt/gentoo/etc/sudoers
+cp $BASEDIR/files/resolv.conf /mnt/gentoo/etc/resolv.conf
+cp $BASEDIR/files/net /mnt/gentoo/etc/conf.d/net
 rm /mnt/gentoo/etc/shadow-
 rm /mnt/gentoo/etc/group-
 rm /mnt/gentoo/etc/gshadow-
 rm /mnt/gentoo/etc/passwd-
 
 cd /mnt/gentoo
-/root/mkstage4.sh -q -l -t . /root/$STAGE4
-cd /root
+$BASEDIR/mkstage4.sh -q -l -t . $BASEDIR/$STAGE4
+cd $BASEDIR
 umount /mnt/gentoo/boot
 umount /mnt/gentoo
 losetup -D
