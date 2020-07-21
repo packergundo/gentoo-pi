1768a3cdc213fecbeaf851ff078aaf100d179a9e
packergundo <packergundo@gmail.com>
2020-07-21 12:22:25 -0500

    Updated update.sh to do eix, match the portage files, and do a system-wide update

--- update.sh
+++ update.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 BASEDIR=/home/pi/git/gentoo-pi
+
+echo "updating Portage repo"
+sudo /usr/bin/eix-sync
+
+echo "updating git repo"
 cd $BASEDIR
+cd /home/pi/git/gentoo-pi
 git pull
 
+echo "Moving files over to /etc/portage"
+cd /home/pi/git/gentoo-pi/files/portage
 sudo cp $BASEDIR/files/portage/make.conf /etc/portage/make.conf
 sudo cp $BASEDIR/files/portage/package.accept_keywords /etc/portage/package.accept_keywords/monolithic
 sudo cp $BASEDIR/files/portage/package.mask /etc/portage/package.mask/monolithic
 sudo cp $BASEDIR/files/portage/package.unmask /etc/portage/package.unmask/monolithic
 sudo cp $BASEDIR/files/portage/package.use /etc/portage/package.use/monolithic
+
+echo "updating the system"
+sudo emerge -auDN world
